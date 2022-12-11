1dec40aba286eb765c22b7d0ffe88a7bacb8121f
Python-boi <Locklairalex@gmail.com>
2022-12-11 14:12:37 -0500

    Added PID calculate function.
    Added PID calculate Function and worked out some formatting.

---
+++ VexSpinUp/SpinUp9447A/include/misc.hpp
@@ -0,0 +1,5 @@
+#pragma once 
+#include "motors.hpp"
+
+double PID::calculate(double target, double current);
+
--- VexSpinUp/SpinUp9447A/src/flywheel.cpp
+++ VexSpinUp/SpinUp9447A/src/flywheel.cpp
@@ -8,19 +8,20 @@ void flywheelVoltage(int speed1){
     flywheel.move_voltage(speed1);
 }
 void toggleFlywheel(){
+  flyWheelState = !flyWheelState;
   if(flywheelstate == true){
     flywheelVoltage(600);
   }
   else{
     flywheel.move_brake();
   }
-  flyWheelState = !flyWheelState;
+  
 }
 void flywheelOpcontrol(){
   if(master.get_digital_new_press(CONTROLLER_DIGITAL_R1) && !lastPress){
 		toggleFlywheel(!flyWheelState);
 	   }
-	   lastPress = master.get_digital_new_press(CONTROLLER_DIGITAL_R1);
+	   
 
    if(master.get_digital_new_press(CONTROLLER_DIGITAL_L1)){
     loader.set_value(!state);
